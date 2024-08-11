81b9435a72c6b73aa25a17bc50eae1775471e949
Kyle Lambert <49013078+kyle-lambert@users.noreply.github.com>
2024-08-11 20:24:31 +0930

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RootPage() {
+  return <div className="p-8">RootPage</div>;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
