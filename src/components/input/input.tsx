08a553ebfe610676949e50124a968609f26c58f1
Kyle Lambert <49013078+kyle-lambert@users.noreply.github.com>
2024-08-16 19:29:44 +0930

    Fix input style

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -10,7 +10,7 @@ import { cn } from "@/utils/cn";
 import { StylePropOverides } from "@/utils/types";
 
 const inputStyles = cva(
-  "rounded-xl border border-gray-200 bg-white leading-5 text-gray-800 placeholder-gray-400 shadow-sm outline-none transition-all invalid:border-rose-500 invalid:ring-rose-100 invalid:hover:border-rose-600 focus:ring invalid:focus:border-rose-500 invalid:focus:ring-rose-100 enabled:hover:border-gray-300 enabled:focus:border-gray-300 enabled:focus:ring-gray-100 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400",
+  "rounded-xl border border-gray-200 bg-white leading-5 text-gray-800 placeholder-gray-400 shadow-sm outline-none transition-all invalid:border-rose-500 invalid:ring-rose-100 invalid:hover:border-rose-600 focus:ring invalid:focus:border-rose-600 invalid:focus:ring-rose-100 enabled:hover:border-gray-300 enabled:focus:border-gray-300 enabled:focus:ring-gray-100 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400",
   {
     variants: {
       size: {
